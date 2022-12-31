9b710e89987894509f9ee8efe26ad47b42f487ba
conarin <53005059+conarin@users.noreply.github.com>
2022-12-31 22:13:37 +0900

    test: デバッグ用のconsole.log()を削除

---
+++ events/interactionCreate.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+module.exports = {
+    name: Discord.Events.InteractionCreate,
+    once: false,
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
+
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.log(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+};

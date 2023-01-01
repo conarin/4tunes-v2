25bf249a86a52aa633c33be9467d2ac40313473b
conarin <53005059+conarin@users.noreply.github.com>
2023-01-02 02:49:51 +0900

    feat: 例外処理を追加

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+require('dotenv').config();
+const env = process.env;
 module.exports = {
     name: 'sendMessage',
     async send(message, options) {
@@ -17,6 +19,51 @@ module.exports = {
             stickers: options.stickers !== undefined && typeof options.stickers === 'object' ? options.stickers : null,
             reply: replyOptions,
             allowedMentions: {repliedUser: false}
+        }).catch(async error => {
+            console.error(error);
+            console.log('options: ' + JSON.stringify(options, null, 2));
+
+            let iconURL = client.user.avatarURL({format: 'png', dynamic: true, size: 128}),
+                channelName = client.user.username,
+                guildName = 'DM';
+
+            if (message.channel.type !== 'DM') {
+                iconURL = message.guild.iconURL({format: 'png', dynamic: true, size: 128});
+                channelName = message.channel.name;
+                guildName = message.guild.name;
+            }
+
+            const embed = {
+                color: 0xf04747,
+                title: `例外発生`,
+                author: {
+                    name: message.author.tag,
+                    icon_url: message.author.avatarURL({format: 'png', dynamic: true, size: 128}),
+                    url: message.author.avatarURL({format: 'png', dynamic: true, size: 128}),
+                },
+                description: `${message.content}\n\`\`\`${error.stack}\`\`\``,
+                timestamp: new Date(),
+                footer: {
+                    text: `\n${channelName} in ${guildName}`,
+                    icon_url: iconURL
+                }
+            };
+            console.log(embed);
+
+            const logChannel = await client.channels.fetch(env.LOG_CHANNEL_ID);
+            if (logChannel) await logChannel
+                .send({content:`<@${(await client.application.fetch()).owner.id}>`, embeds: [embed]})
+                .catch(error => console.error(error));
+
+            await message.channel.send({embeds: [{
+                    color: 0xf04747,
+                    title: '予期しない例外が発生しました',
+                }],
+                reply: replyOptions,
+                allowedMentions: {repliedUser: false}
+            }).catch(err => {
+                if (err.message !== 'Missing Permissions') console.log(err.stack);
+            });
         });
     }
 };

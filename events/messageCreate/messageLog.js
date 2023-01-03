12b3297a5a61250bb11b32a1760d0956a516b80d
conarin <53005059+conarin@users.noreply.github.com>
2023-01-03 22:00:01 +0900

    feat: 発言数用の処理を追加

--- events/messageCreate/messageLog.js
+++ events/messageCreate/messageLog.js
@@ -1,8 +1,14 @@
 const sendMessage = require('../../utils/sendMessage.js');
+const fourTunesAPI = require("../../utils/4TunesAPI");
 module.exports = {
     name: 'messageLog',
     guildOnly: true,
     async execute(message, data) {
+        await fourTunesAPI.post(`/guilds/${message.guild.id}/members/${message.author.id}/messages`, {
+            channel_id: message.channel.id,
+            message_id: message.id
+        });
+
         if (!data.guildData.log_channel_id) return;
 
         const iconURL = message.guild.iconURL({format: 'png', dynamic: true, size: 128}),

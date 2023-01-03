c3a452c9c9c62cbf85f36888368bbc8f2e0e3dd4
conarin <53005059+conarin@users.noreply.github.com>
2023-01-03 21:47:47 +0900

    feat: ギルドでの発言の場合ギルドデータとメンバーデータを取得するように

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,12 +1,26 @@
 const Discord = require('discord.js');
+const fourTunesAPI = require('../utils/4TunesAPI.js');
 module.exports = {
     name: Discord.Events.MessageCreate,
     once: false,
     async execute(message) {
         if (message.author.bot) return;
 
+        const data = {};
+        if (message.guild && message.guild?.available) {
+            data.guildData = await fourTunesAPI.fetch('/guilds', message.guild.id, {
+                guild_id: message.guild.id
+            });
+            if (data.guildData === undefined) return;
+
+            data.memberData = await fourTunesAPI.fetch(`/guilds/${message.guild.id}/members`, message.author.id, {
+                user_id: message.author.id
+            });
+            if (data.memberData === undefined) return;
+        }
+
         for (const handle of [...client.messageHandles.values()]) {
-            await handle.execute(message);
+            await handle.execute(message, data);
         }
     }
 };

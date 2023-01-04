d320cd19a8921dc7c55ddcd140e5611797ebdeeb
conarin <53005059+conarin@users.noreply.github.com>
2023-01-05 01:46:35 +0900

    feat: deleteコマンドを実装

---
+++ commands/delete.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const replyInteraction = require('../utils/replyInteraction.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('delete')
+        .setDescription('メッセージを指定数削除する')
+        .addIntegerOption(option => option
+            .setName('number')
+            .setDescription('削除する件数')
+            .setRequired(true)
+            .setMinValue(1)
+            .setMaxValue(100)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .setDMPermission(false),
+    async execute(interaction) {
+        const number = Math.floor(interaction.options.getInteger('number'));
+
+        interaction.channel.bulkDelete(number, true)
+            .then(messages => {
+                replyInteraction.reply(interaction, {
+                    embeds: [{
+                        title: '成功しました',
+                        description: `${messages.size} 件のメッセージを削除しました`,
+                        color: client.colors.success
+                    }],
+                    ephemeral: true
+                });
+            })
+            .catch(error => {
+                console.error(error);
+                replyInteraction.reply(interaction, {
+                    embeds: [{
+                        title: '失敗しました',
+                        description: error.status === 403 ?
+                            'メッセージ管理権限がないため削除できませんでした' : '適切な権限が付与されているか確認してください',
+                        color: client.colors.danger
+                    }],
+                    ephemeral: true
+                });
+            });
+    },
+};

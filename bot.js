a00e274fa9d1a86105f0fa0697b270df563579a8
conarin <53005059+conarin@users.noreply.github.com>
2023-01-05 00:02:21 +0900

    feat: 埋め込みに使用する色を定義

--- bot.js
+++ bot.js
@@ -19,3 +19,10 @@ client.login(env.DISCORD_TOKEN);
 require("./init/setCommands.js").execute();
 require("./init/setEvents.js").execute();
 require("./init/setMessageHandles.js").execute();
+
+client.colors = {
+    green: 0x43b581,
+    orange: 0xfaa61a,
+    red: 0xf04747,
+    blue: 0x3498db
+};

1843c8488bc075ae96e16f373778f6eace086976
conarin <53005059+conarin@users.noreply.github.com>
2023-01-04 18:11:00 +0900

    feat: guildCommandsに対応

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,9 @@ const fs = require('node:fs');
 require('dotenv').config();
 const env = process.env;
 
-const deleteCommandIds = [];
+const deleteCommands = [
+    // {id: 'commandId', guildId: null}
+];
 
 const commands = [];
 
@@ -13,7 +15,24 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
+    commands.push({
+        data: command.data.toJSON(),
+        guilds: command.guilds
+    });
+}
+
+const applicationCommands = commands.filter(command => !command.guilds).map(command => command.data);
+console.log('application commands: ' + JSON.stringify(applicationCommands, null, 2));
+
+let applicationGuildCommands = {};
+commands.filter(command => command.guilds).forEach(command => {
+    command.guilds.map(guildId => {
+        if (!applicationGuildCommands[guildId]) applicationGuildCommands[guildId] = [];
+        applicationGuildCommands[guildId].push(command.data);
+    });
+});
+for (const guildId in applicationGuildCommands) {
+    console.log(guildId + ': ' + JSON.stringify(applicationGuildCommands[guildId], null, 2));
 }
 
 const rest = new Discord.REST({ version: '10' }).setToken(env.DISCORD_TOKEN);
@@ -22,22 +41,38 @@ const rest = new Discord.REST({ version: '10' }).setToken(env.DISCORD_TOKEN);
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(
-            // Discord.Routes.applicationGuildCommands(env.CLIENT_ID, env.DEV_GUILD_ID),
-            Discord.Routes.applicationCommands(env.CLIENT_ID),
-            { body: commands },
-        );
+        if (applicationCommands.length) {
+            // The put method is used to fully refresh all commands in the guild with the current set
+            const data = await rest.put(
+                Discord.Routes.applicationCommands(env.CLIENT_ID),
+                {body: applicationCommands},
+            );
+            console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+        }
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+        if (Object.keys(applicationGuildCommands).length) {
+            for (const guildId in applicationGuildCommands) {
+                const data = await rest.put(
+                    Discord.Routes.applicationGuildCommands(env.CLIENT_ID, guildId),
+                    { body: applicationGuildCommands[guildId] },
+                );
+                console.log(`Successfully reloaded ${data.length} application guild (/) commands.`);
+            }
+        }
     } catch (error) {
         // And of course, make sure you catch and log any errors!
         console.error(error);
     }
 })();
 
-deleteCommandIds.forEach(id => {
-    rest.delete(Discord.Routes.applicationCommand(env.CLIENT_ID, id))
-        .then(() => console.log('Successfully deleted application command'))
-        .catch(console.error);
+deleteCommands.forEach(command => {
+    if (command.guildId) {
+        rest.delete(Discord.Routes.applicationGuildCommand(env.CLIENT_ID, command.guildId, command.id))
+            .then(() => console.log(`Successfully deleted application command: ${command.id}`))
+            .catch(console.error);
+    } else {
+        rest.delete(Discord.Routes.applicationCommand(env.CLIENT_ID, command.id))
+            .then(() => console.log(`Successfully deleted application command: ${command.id}`))
+            .catch(console.error);
+    }
 });

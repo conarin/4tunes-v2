3c22a39a382b64edb7681ff612a5782ff8a166ee
conarin <53005059+conarin@users.noreply.github.com>
2023-01-04 21:01:01 +0900

    feat: avatarコマンドを実装

---
+++ commands/avatar.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+const replyInteraction = require('../utils/replyInteraction.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('指定したユーザーのアバターを表示する')
+        .addUserOption(option => option
+            .setName('user')
+            .setDescription('表示したいユーザーを選択')
+            .setRequired(false)
+        ),
+    async execute(interaction) {
+        let user = interaction.options.getUser('user');
+        if (!user) user = interaction.user;
+
+        await replyInteraction.reply(interaction, {
+            embeds: [{
+                author: {
+                    name: `${user.tag}\n(${user.id})`
+                },
+                image: {
+                    url: user.displayAvatarURL({ format: 'png', dynamic: true, size:2048 })
+                },
+            }]
+        });
+    },
+};

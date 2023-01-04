2e60079d9841d98fe2da998f0ae0639b15083fad
conarin <53005059+conarin@users.noreply.github.com>
2023-01-04 18:40:00 +0900

    feat: interactionの返信処理を実装

---
+++ utils/replyInteraction.js
@@ -0,0 +1,60 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+const env = process.env;
+module.exports = {
+    name: 'sendMessage',
+    async reply(interaction, options) {
+        if (!options) return;
+
+        await interaction.reply({
+            content: options.content !== undefined ? String(options.content) : null,
+            embeds: options.embeds !== undefined && typeof options.embeds === 'object' ? options.embeds : null,
+            files: options.files !== undefined && typeof options.files === 'object' ? options.files : null,
+            components: options.components !== undefined && typeof options.components === 'object' ? options.components : null
+        }).catch(async error => {
+            console.error(error);
+            console.log('options: ' + JSON.stringify(options, null, 2));
+            console.log('interaction.options: ' + JSON.stringify(interaction.options, null, 2));
+
+            let iconURL = client.user.avatarURL({format: 'png', dynamic: true, size: 128}),
+                channelName = client.user.username,
+                guildName = 'DM';
+
+            if (interaction.channel.type !== Discord.ChannelType.DM) {
+                iconURL = interaction.guild.iconURL({format: 'png', dynamic: true, size: 128});
+                channelName = interaction.channel.name;
+                guildName = interaction.guild.name;
+            }
+
+            const embed = {
+                color: 0xf04747,
+                title: `例外発生`,
+                author: {
+                    name: interaction.user.tag,
+                    icon_url: interaction.user.avatarURL({format: 'png', dynamic: true, size: 128}),
+                    url: interaction.user.avatarURL({format: 'png', dynamic: true, size: 128}),
+                },
+                description: `${interaction.commandName}\n\`\`\`${error.stack}\`\`\``,
+                timestamp: new Date(),
+                footer: {
+                    text: `\n${channelName} in ${guildName}`,
+                    icon_url: iconURL
+                }
+            };
+
+            const logChannel = await client.channels.fetch(env.LOG_CHANNEL_ID).catch(error => console.error(error));
+            const clientApplication = await client.application.fetch();
+            if (logChannel) await logChannel
+                .send({content:`<@${clientApplication.owner.ownerId || clientApplication.owner.id}>`, embeds: [embed]})
+                .catch(error => console.error(error));
+
+            await interaction.reply({embeds: [{
+                    color: 0xf04747,
+                    title: '予期しない例外が発生しました',
+                }]
+            }).catch(err => {
+                if (err.message !== 'Missing Permissions') console.log(err.stack);
+            });
+        });
+    }
+};

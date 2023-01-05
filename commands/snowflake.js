c029cee597ded86f4bff2b8c5d29a60cbb3f86ad
conarin <53005059+conarin@users.noreply.github.com>
2023-01-06 03:45:38 +0900

    feat: snowflakeコマンドを実装

---
+++ commands/snowflake.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { DiscordSnowflake } = require('@sapphire/snowflake');
+const RE2 = require('re2');
+const replyInteraction = require('../utils/replyInteraction.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('snowflake')
+        .setDescription('snowflakeの作成・日時への変換を行う')
+        .addSubcommand(option => option
+            .setName('generate')
+            .setDescription('snowflakeを作成する')
+        )
+        .addSubcommand(option => option
+            .setName('date')
+            .setDescription('snowflakeから日時を求める')
+            .addStringOption(option => option
+                .setName('snowflake')
+                .setDescription('snowflake')
+                .setRequired(true)
+            )
+        ),
+    async execute(interaction) {
+        const subcommand = interaction.options.getSubcommand();
+        if (subcommand === 'generate') {
+            await replyInteraction.reply(interaction, {
+                content: DiscordSnowflake.generate()
+            });
+        } else if (subcommand === 'date') {
+            let snowflake = interaction.options.getString('snowflake');
+
+            if (!new RE2(/^\d+$/).test(snowflake)) {
+                await replyInteraction.reply(interaction, {
+                    embeds: [{
+                        title: '無効な値',
+                        description: 'snowflakeの形式で入力してください',
+                        color: client.colors.warning
+                    }]
+                });
+                return;
+            }
+
+            const timestamp = Math.floor(Number(DiscordSnowflake.deconstruct(snowflake).timestamp) / 1000);
+            await replyInteraction.reply(interaction, {
+                content: `<t:${timestamp}:d><t:${timestamp}:T>`
+            });
+        }
+    }
+};

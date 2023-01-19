2287c034f2b27f90ce0ca7069c27dd6fe8914c19
conarin <53005059+conarin@users.noreply.github.com>
2023-01-19 23:32:06 +0900

    refactor: 不要な処理を削除

--- utils/calculator.js
+++ utils/calculator.js
@@ -2,29 +2,18 @@ const RE2 = require('re2');
 const {rpn, Generate} = require('./rpn.js');
 
 const toHalfWidth = strVal => {
-    // 半角変換
-    const halfVal = strVal.replace(/[！-～]/g,
-        function( tmpStr ) {
-            // 文字コードをシフト
-            return String.fromCharCode( tmpStr.charCodeAt(0) - 0xFEE0 );
-        }
-    );
+    const halfVal = strVal.replace(new RE2(/[！-～]/g), tmpStr => {
+        return String.fromCharCode(tmpStr.charCodeAt(0) - 0xFEE0);
+    });
 
-    // 文字コードシフトで対応できない文字の変換
-    return halfVal.replace(/”/g, "\"")
-        .replace(/’/g, "'")
-        .replace(/‘/g, "`")
-        .replace(/￥/g, "\\")
-        .replace(/　/g, " ")
-        .replace(/Π/g, "π")
-        .replace( /\s+/g,'' )
-        .replace(/〜/g, "~");
+    return halfVal
+        .replace(new RE2(/　/g), " ")
+        .replace(new RE2(/\s+/g),'' );
 };
 
 module.exports = {
     calculate(formula) {
-        formula = toHalfWidth(formula);
-        formula = formula.toLowerCase();
+        formula = toHalfWidth(formula).toLowerCase();
 
         const operandRegex = new RE2(/^[0-9.]+$/),
             operatorRegex = new RE2(/^[+\-×*÷/^%()=]+$/),

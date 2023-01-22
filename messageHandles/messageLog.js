8cbfdc487417e121dd19c2cc9c7eb1f4a5289e65
conarin <53005059+conarin@users.noreply.github.com>
2023-01-23 01:48:21 +0900

    refactor: guildMessageLog側で処理をするように

--- messageHandles/messageLog.js
+++ messageHandles/messageLog.js
@@ -3,12 +3,12 @@ const fourTunesAPI = require("../utils/4TunesAPI");
 module.exports = {
     name: 'messageLog',
     guildOnly: true,
-    async execute(message, data) {
+    async execute(message) {
         await fourTunesAPI.post(`/guilds/${message.guild.id}/members/${message.author.id}/messages`, {
             channel_id: message.channel.id,
             message_id: message.id
         });
 
-        await guildMessageLog.execute(message, data.guildData.log_channel_id, client.colors.success);
+        await guildMessageLog.created(message);
     }
 };
--- utils/guildMessageLog.js
+++ utils/guildMessageLog.js
@@ -1,24 +1,44 @@
+const fourTunesAPI = require("./4TunesAPI");
 const Message = require("./message");
 require('date-utils');
 module.exports = {
-    async execute(message, logChannelId, color) {
-        if (!logChannelId) return;
-
+    async fetchGuildData(message) {
+        if (message.guild && message.guild?.available) {
+            return await fourTunesAPI.fetch('/guilds', message.guild.id, {
+                guild_id: message.guild.id
+            });
+        }
+    },
+    createFooter(message) {
         const iconURL = message.guild.iconURL({format: 'png', dynamic: true, size: 128}),
             channelName = message.channel.name,
             guildName = message.guild.name;
 
+        return {
+            text: `${channelName} in ${guildName}\n${new Date().toFormat('YYYY/MM/DD HH24:MI:SS')}`,
+            icon_url: iconURL
+        };
+    },
+    async send(message, options) {
+        const guildData = await this.fetchGuildData(message);
+        if (!guildData || !guildData.log_channel_id) return;
+
+        const logChannel = await client.channels.fetch(guildData.log_channel_id).catch(error => {
+            if (error.status !== 404) console.error(error);
+        });
+        if (!logChannel) return;
+
+        await Message.send(message, logChannel, options);
+    },
+    async createdOrEdited(message, color) {
         const contentEmbed = [{
             color: color,
             description: `[${message.content}](${message.url})`,
             author: {
                 name: `${message.author.tag}\n(${message.author.id})`,
-                icon_url: message.author.displayAvatarURL({format: 'png', dynamic: true, size:128})
+                icon_url: message.author.displayAvatarURL({format: 'png', dynamic: true, size: 128})
             },
-            footer: {
-                text: `${channelName} in ${guildName}\n${new Date().toFormat('YYYY/MM/DD HH24:MI:SS')}`,
-                icon_url: iconURL
-            }
+            footer: this.createFooter(message)
         }];
 
         const options = {
@@ -28,11 +48,24 @@ module.exports = {
             stickers: [...message.stickers.values()] || null
         };
 
-        const logChannel = await client.channels.fetch(logChannelId).catch(error => {
-            if (error.status !== 404) console.error(error);
-        });
-        if (!logChannel) return;
+        await this.send(message, options);
+    },
+    async created(message) {
+        await this.createdOrEdited(message, client.colors.success);
+    },
+    async edited(message) {
+        await this.createdOrEdited(message, client.colors.warning);
+    },
+    async deleted(message) {
+        const options = {
+            embeds: [{
+                color: client.colors.danger,
+                title: 'messageDelete',
+                description: `[${message.id}](${message.url})`,
+                footer: this.createFooter(message)
+            }]
+        };
 
-        await Message.send(message, logChannel, options);
+        await this.send(message, options);
     }
 };

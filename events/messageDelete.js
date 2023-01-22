f81c0637a77b6b0eff35564913f4d4613f6e6d39
conarin <53005059+conarin@users.noreply.github.com>
2023-01-23 01:53:02 +0900

    feat: ログ機能のメッセージ削除時の処理を追加

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,1 +1,9 @@
-// 削除ログ
+const Discord = require("discord.js");
+const guildMessageLog = require("../utils/guildMessageLog");
+module.exports = {
+    name: Discord.Events.MessageDelete,
+    once: false,
+    async execute(message) {
+        await guildMessageLog.deleted(message);
+    }
+};

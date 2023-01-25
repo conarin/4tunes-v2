8a75226e8d9610317bfbe23f0bf74e40c8a1c1af
conarin <53005059+conarin@users.noreply.github.com>
2023-01-26 00:11:13 +0900

    test: イベント処理で予期せぬ例外が発生した場合の処理を追加

--- init/setEvents.js
+++ init/setEvents.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const fs = require("fs");
+require('dotenv').config();
+const env = process.env;
 module.exports = {
     execute() {
         const eventsPath = path.join(__dirname, '../events');
@@ -8,9 +10,23 @@ module.exports = {
             const filePath = path.join(eventsPath, file);
             const event = require(filePath);
             if (event.once) {
-                client.once(event.name, async (...args) => event.execute(...args));
+                client.once(event.name, async (...args) => {
+                    try {
+                        event.execute(...args)
+                    } catch (error) {
+                        console.error(error);
+                        console.log(`<@${env.CLIENT_APP_OWNER_ID}>`);
+                    }
+                });
             } else {
-                client.on(event.name, async (...args) => event.execute(...args));
+                client.on(event.name, async (...args) => {
+                    try {
+                        event.execute(...args)
+                    } catch (error) {
+                        console.error(error);
+                        console.log(`<@${env.CLIENT_APP_OWNER_ID}>`);
+                    }
+                });
             }
         }
     }

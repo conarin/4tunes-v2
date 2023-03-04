cc295d9a6e19f0730da14023a0b6288b14a2b58a
conarin <53005059+conarin@users.noreply.github.com>
2023-03-05 02:11:20 +0900

    feat: console.error()に変更

--- utils/messages.js
+++ utils/messages.js
@@ -20,19 +20,19 @@ module.exports = {
 
             const guild = await client.guilds.fetch(results.guildId).catch(error => {
                 results.status = error.status;
-                if (error.status !== 403) console.log('quoteGuildFetchError: ' + error);
+                if (error.status !== 403) console.error('GuildFetchError: ' + error);
             });
             if (!guild) return results;
 
             const channel = await client.channels.fetch(results.channelId).catch(error => {
                 results.status = error.status;
-                if (error.status !== 404) console.log('quoteChannelFetchError: ' + error);
+                if (error.status !== 404) console.error('ChannelFetchError: ' + error);
             });
             if (!channel) return results;
 
             let message = await channel.messages.fetch(results.messageId).catch(error => {
                 results.status = error.status;
-                if (error.status !== 404) console.log('quoteMessageFetchError: ' + error);
+                if (error.status !== 404) console.error('MessageFetchError: ' + error);
             });
             if (!message) return results;
 
